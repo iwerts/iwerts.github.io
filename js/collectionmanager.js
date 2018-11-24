68798534d9be3c3a2edc8b5fd99500fa3f819861
Ian Werts <ian.werts@gmail.com>
2018-11-24 18:10:42 -0500

    Updated CollectionManager

--- js/collectionmanager.js
+++ js/collectionmanager.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    alert("TEST");
+$.getJSON("data/products.json", function(data){
+    var items = [];
+    $.each(data, function(key, val){
+        items.push("<li id='"+key+"'>"+val+"</li>");
+    });
+
+    $("<ul/>",{
+        "class":"my-new-list",
+        html: items.join("")
+    }).appendTo("body");
 });
