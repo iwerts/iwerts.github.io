4a7e30b69c2bf294ab2151087cdeaae995aeb124
Ian Werts <ian.werts@gmail.com>
2018-11-24 22:35:33 -0500

    Dynamic Homepage

--- js/app.js
+++ js/app.js
@@ -1,40 +1,9 @@
-
-jQuery(document).ready(function(){
-    $(document).foundation()
-
-    // This button will increment the value
-    $('[data-quantity="plus"]').click(function(e){
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('data-field');
-        // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
-        // If is not undefined
-        if (!isNaN(currentVal)) {
-            // Increment
-            $('input[name='+fieldName+']').val(currentVal + 1);
-        } else {
-            // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
-        }
+$(document).foundation()
+$.getJSON("data/products.json", function(data){
+    var items = [];
+    $.each(data, function(key, val){
+        items.push("<div class=\"column\">\n<img class=\"thumbnail\" src=\"https://placehold.it/550x550\">\n<h5>"+val['name']+"</h5>\n</div>");
     });
-    // This button will decrement the value till 0
-    $('[data-quantity="minus"]').click(function(e) {
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('data-field');
-        // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
-        // If it isn't undefined or its greater than 0
-        if (!isNaN(currentVal) && currentVal > 0) {
-            // Decrement one
-            $('input[name='+fieldName+']').val(currentVal - 1);
-        } else {
-            // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
-        }
-    });
+    
+    $('#my_list').append.apply($('#my_list'), items);
 });
-
