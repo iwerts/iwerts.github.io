7bd5b6a713a85603183b5d98cc127986debe3e3e
Ian Werts <ian.werts@gmail.com>
2018-11-24 19:14:55 -0500

    Updated CollectionManager

--- js/collectionmanager.js
+++ js/collectionmanager.js
@@ -2,6 +2,13 @@ $.getJSON("data/products.json", function(data){
     var items = [];
     $.each(data, function(key, val){
         items.push("<li id='"+key+"'>"+val['name']+"</li>");
+        if (val['ships'].length > 0){
+            items.push("<li><ul>")
+            $.each(val['ships'], function(key, val){
+                items.push("<li>"+val+"</li>");
+            });
+            items.push("</li></ul>")
+        }
     });
 
     $("<ul/>",{
