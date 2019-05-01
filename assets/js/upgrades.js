a0504445c9abb880c6706110722843cad76470f5
Ian Werts <s278729@corp.aepsc.com>
2019-05-01 09:22:31 -0400

    fdsafdsa

--- assets/js/upgrades.js
+++ assets/js/upgrades.js
@@ -16,6 +16,7 @@ var titles = returnJSON('https://iwerts.github.io/xwing-data2/data/upgrades/titl
 var torpedoes = returnJSON('https://iwerts.github.io/xwing-data2/data/upgrades/torpedo.json');
 var turrets = returnJSON('https://iwerts.github.io/xwing-data2/data/upgrades/turret.json');
 var quickBuild = '';
+var upgrades = [[]];
 function getQuickBuild(){
     for (var i = 0; i < quickBuilds.length; i++){
         for (var j = 0; j < quickBuilds[i].builds.length; j++){
@@ -27,75 +28,78 @@ function getQuickBuild(){
     
     if (quickBuild.pilots[0].upgrades.hasOwnProperty('astromech')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.astromech.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('astromech',quickBuild.pilots[0].upgrades.astromech[i]));
+            
+            upgrades.push([getUpgradeByType('astromech',quickBuild.pilots[0].upgrades.astromech[i]), 0]);
         }
     }
     if (quickBuild.pilots[0].upgrades.hasOwnProperty('cannon')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.cannon.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('cannon',quickBuild.pilots[0].upgrades.cannon[i]));
+            upgrades.push([getUpgradeByType('cannon',quickBuild.pilots[0].upgrades.cannon[i]), 0]);
         }
     }
     if (quickBuild.pilots[0].upgrades.hasOwnProperty('configuration')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.configuration.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('configuration',quickBuild.pilots[0].upgrades.configuration[i]));
+            upgrades.push([getUpgradeByType('configuration',quickBuild.pilots[0].upgrades.configuration[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('crew')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.crew.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('crew',quickBuild.pilots[0].upgrades.crew[i]));
+            upgrades.push([getUpgradeByType('crew',quickBuild.pilots[0].upgrades.crew[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('device')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.device.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('device',quickBuild.pilots[0].upgrades.device[i]));
+            upgrades.push([getUpgradeByType('device',quickBuild.pilots[0].upgrades.device[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('force-power')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades['force-power'].length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('force-power',quickBuild.pilots[0].upgrades.force_powers[i]));
+            upgrades.push([getUpgradeByType('force-power',quickBuild.pilots[0].upgrades.force_powers[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('gunner')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.gunner.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('gunner',quickBuild.pilots[0].upgrades.gunner[i]));
+            upgrades.push([getUpgradeByType('gunner',quickBuild.pilots[0].upgrades.gunner[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('illicit')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.illicit.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('illicit',quickBuild.pilots[0].upgrades.illicit[i]));
+            upgrades.push([getUpgradeByType('illicit',quickBuild.pilots[0].upgrades.illicit[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('missile')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.missile.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('missile',quickBuild.pilots[0].upgrades.missile[i]));
+            upgrades.push([getUpgradeByType('missile',quickBuild.pilots[0].upgrades.missile[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('modification')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.modification.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('modification',quickBuild.pilots[0].upgrades.modification[i]));
+            upgrades.push([getUpgradeByType('modification',quickBuild.pilots[0].upgrades.modification[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('sensor')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.sensor.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('sensor',quickBuild.pilots[0].upgrades.sensor[i]));
+            upgrades.push([getUpgradeByType('sensor',quickBuild.pilots[0].upgrades.sensor[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('tactical-relay')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades['tactical-relay'].length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('tactical-relay',quickBuild.pilots[0].upgrades.tactical_relays[i]));
+            upgrades.push([getUpgradeByType('tactical-relay',quickBuild.pilots[0].upgrades.tactical_relays[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('talent')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.talent.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('talent',quickBuild.pilots[0].upgrades.talent[i]));
+            upgrades.push([getUpgradeByType('talent',quickBuild.pilots[0].upgrades.talent[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('tech')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.tech.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('tech',quickBuild.pilots[0].upgrades.tech[i]));
+            upgrades.push([getUpgradeByType('tech',quickBuild.pilots[0].upgrades.tech[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('title')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.title.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('title',quickBuild.pilots[0].upgrades.title[i]));
+            upgrades.push([getUpgradeByType('title',quickBuild.pilots[0].upgrades.title[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('torpedo')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.torpedo.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('torpedo',quickBuild.pilots[0].upgrades.torpedo[i]));
+            upgrades.push([getUpgradeByType('torpedo',quickBuild.pilots[0].upgrades.torpedo[i]), 0]);
         }
     }if (quickBuild.pilots[0].upgrades.hasOwnProperty('turret')){
         for (var i = 0; i < quickBuild.pilots[0].upgrades.turret.length; i++){
-            addUpgradeToPilotTextBox(getUpgradeByType('turret',quickBuild.pilots[0].upgrades.turret[i]));
+            upgrades.push([getUpgradeByType('turret',quickBuild.pilots[0].upgrades.turret[i]), 0]);
         }
     }
+
+    displayUpgrades();
 }
 
 function getUpgradeByType(upgradeType, upgradeXWS){
@@ -138,8 +142,13 @@ function getUpgradeByType(upgradeType, upgradeXWS){
     }
 }
 
+function displayUpgrades(){
+    for (var i = 0; i < upgrades.length; i++){
+        addUpgradeToPilotTextBox(upgrades[i][0]);
+    }
+}
+
 function addUpgradeToPilotTextBox(upgrade){
-    console.log(upgrade);
     $('#pilot_text_box').append('<hr><span id="upgrade_ability">'+getUpgradeName(upgrade)+ getUpgradeText(upgrade)+'</span>');
 }
 
@@ -168,10 +177,13 @@ function getUpgradeName(upgrade){
 function getUpgradeText(upgrade){
     upgradeText = '';
     if (upgrade.hasOwnProperty('sides')){
+        if (upgrade.sides.length > 1){
+
+        }
         if (upgrade.sides[0].hasOwnProperty('ability')){
             upgradeText += convertGameText(upgrade.sides[0].ability);
         }
-        
+    
     if (upgrade.sides[0].hasOwnProperty('force')){
         console.log(upgrade.sides[0].force);
         if ($('#force_stat_value').length == 0){
@@ -182,6 +194,13 @@ function getUpgradeText(upgrade){
                 }
             }
             $('#pilot_stats').append('<div class="pilot_stat"><span class="pilot_stat_icon">' + convertIcon('forcecharge')+' </span><span class="pilot_stat_value" id="force_stat_value">'+value + '</span></div>');
+        } else {
+            value = upgrade.sides[0] + $('#force_stat_value').text().substring(0,1);
+            if (upgrade.sides[0].force.hasOwnProperty('recovers') || current_pilot.force.hasOwnProperty('recovers')){
+                for (var i = 0; i < current_pilot.force.recovers + upgrade.sides[0].force.recovers; i++){
+                    value += '⯅';
+                }
+            }
         }
     }
     }
