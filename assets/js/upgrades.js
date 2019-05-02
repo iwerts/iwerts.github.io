d3a8849e3b7b6251818d3984a1ed58d320219fce
Ian Werts <s278729@corp.aepsc.com>
2019-05-02 10:39:07 -0400

    fdsa

--- assets/js/upgrades.js
+++ assets/js/upgrades.js
@@ -243,7 +243,7 @@ function getUpgradeActions(upgrade, side){
         
         for (var i = 0; i < upgrade.sides[side].actions.length; i++){
             if (upgrade.sides[side].actions[i].hasOwnProperty('linked')){
-                $('#pilot_actions').append('<span class="upgrade_action">' + convertIcon(upgrade.sides[side].actions[i].type.toLowerCase().replace(/\s/g, ''), action.difficulty) + ' ' + convertIcon('linked', upgrade.sides[side].actions[i].difficulty) + ' ' + convertIcon(upgrade.sides[side].actions[i].linked.type.toLowerCase().replace(/\s/g, ''), upgrade.sides[side].actions[i].linked.difficulty) + '</span>');
+                $('#pilot_actions').append('<span class="upgrade_action">' + convertIcon(upgrade.sides[side].actions[i].type.toLowerCase().replace(/\s/g, ''), upgrade.sides[side].actions[i].difficulty) + ' ' + convertIcon('linked', upgrade.sides[side].actions[i].difficulty) + ' ' + convertIcon(upgrade.sides[side].actions[i].linked.type.toLowerCase().replace(/\s/g, ''), upgrade.sides[side].actions[i].linked.difficulty) + '</span>');
             } else {
                 $('#pilot_actions').append('<span class="upgrade_action">' + convertIcon(upgrade.sides[side].actions[i].type.toLowerCase().replace(/\s/g, ''), upgrade.sides[side].actions[i].difficulty) + '</span>');
             }
