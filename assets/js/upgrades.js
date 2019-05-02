58f9aeb739ba4667af6268f365b1823f6eb11c90
Ian Werts <s278729@corp.aepsc.com>
2019-05-02 12:53:50 -0400

    gfdsgf

--- assets/js/upgrades.js
+++ assets/js/upgrades.js
@@ -199,7 +199,7 @@ function getUpgradeAttack(upgrade, side){
             ordnance = convertText('rangebonusindicator');
             return '<span class="attack_upgrade">'+arc + ' ' + value + '<br>' + ordnance + minRange + ' - ' + maxRange + '</span>';
         } else {
-            $('#pilot_attack').append('<div class="upgrade_stat"><span class="upgrade_stat_icon">'+arc+'</span><span class="upgrade_stat_value">'+value+'</span><br><span class="attack_range">'+'('+minRange + ' - ' + maxRange + ')</span></div>');
+            $('#pilot_attack').append('<div class="upgrade_stat"><span class="upgrade_stat_icon">'+arc+'</span><span class="upgrade_stat_value">'+value+'</span><span class="attack_range">'+'('+minRange + ' - ' + maxRange + ')</span></div>');
             return '';
         }
        
