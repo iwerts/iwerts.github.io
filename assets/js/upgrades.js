b737819efd1c689a9517240d7671386978ebe7b4
Ian Werts <s278729@corp.aepsc.com>
2019-05-07 11:34:46 -0400

    hjklhlkj

--- assets/js/upgrades.js
+++ assets/js/upgrades.js
@@ -251,7 +251,7 @@ function getUpgradeGrants(upgrade, side){
                 var current_value = $(stat_type).text();
                 if ($(stat_type).length == 0){
                     console.log("DOESN'T HAVE " + stat_type + "!");
-                }
+                } 
                 var modification_value = upgrade.sides[side].grants[i].amount;
                 
                 var total_value = parseInt(current_value)+parseInt(modification_value);
